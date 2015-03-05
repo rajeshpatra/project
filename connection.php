888dec869eed2f4ca259749dabaf4a182e2358ab
Rajesh Patra <p7.rajesh@gmail.com>
2015-03-06 02:28:27 +0530

    connection working properly after changing to die(); from die ;.

--- connection.php
+++ connection.php
@@ -3,14 +3,14 @@
 	require "constants.php";
 	$connection = mysql_connect(DB_SERVER, DB_USER, DB_PASS);
 		if(!$connection){
-			die "could not connect to db." . mysql_error();
+			die ("could not connect to db." . mysql_error());
 		}else{
 			echo "db connection successfull";
 		}
 
 	$db = mysql_select_db(DB_NAME, $connection);
 		if(!$db){
-			die "could not select db." . mysql_error();
+			die ("could not select db." . mysql_error());
 		}else{
 			echo "db selection successfull";
 		}
