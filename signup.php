1ad54268c50bf9bc3f3fc22780fd915ea336da89
Rajesh Patra <p7.rajesh@gmail.com>
2015-03-06 01:37:42 +0530

    Added php part, though my connection is not working.

--- signup.php
+++ signup.php
@@ -7,7 +7,41 @@
 	<script type="text/javascript" src="dist/js/kickstart.js"></script>
 </head>
 <body>
-<?php require_once "includes/navbar.php"; ?>
+<?php 
+require_once "includes/navbar.php";
+require "connection.php";
+	if(isset($_POST['signin'])){
+		$name = $_POST['name'];
+		$email = $_POST['email'];
+		$mobno = $_POST['mobno'];
+		$pass1 = $_POST['password'];
+		$pass = md5($_POST['confpass']);
+
+		$email_check = "SELECT id FROM data WHERE email='{$email}'";
+		$result_email_check = mysql_query($email_check);
+		if($result_email_check){
+			if(mysql_num_rows($result_email_check) == 1){
+				if($pass1 == $pass){
+					$query = "INSERT INTO data(name, email, mobno, pass) VALUES ('{$name}', '{$email}', '{$mobno}', '{$pass}'";
+					$result_query = mysql_query($query);
+					if(!$result_query){
+						die "data not inserted." . mysql_query();
+					}else{
+						$data = mysql_fetch_assoc($result_query);
+						$_SESSION['username'] = $data['name'];
+						$_SESSION['useremail'] = $data['email'];
+						$_SESSION['usermobno'] = $data['mobno'];
+						header("Location: home.php?registered=1");
+					}
+				}echo "password don't match." mysql_error();
+			}
+		}else{
+			echo "you are not registered." . mysql_error(); 
+		}
+	}
+
+?>
+	
 	<h2>Signup</h2>
 	<form method="post" action="signup.php">
 		<table>
