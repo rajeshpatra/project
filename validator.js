c2b8cd43d4aa49ef1315449b63abb14ba9b14643
Rajesh Patra <p7.rajesh@gmail.com>
2015-03-07 23:48:42 +0530

    renamed .php to .js for this file

---
+++ validator.js
@@ -0,0 +1,39 @@
+function validateForm() {
+
+	if(document.f_signup.name.value == ""){
+	alert("Please provide your name!");
+	document.f_signup.name.focus();
+	return false;
+	}
+
+	emailregexp = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+	if(document.f_signup.email.value == ""){
+	alert("Please provide your email!");
+	document.f_signup.email.focus();
+	return false;
+	}else if(!emailregexp.test(document.f_signup.email.value)){
+	alert("Please enter a valid email!");
+	document.f_signup.email.focus();
+	return false;
+	}
+
+	if(document.f_signup.sex.value == "0"){
+	alert("Please mention your sex!");
+	document.f_signup.sex.focus();
+	return false;
+	}
+
+	if(document.f_signup.mobno.value == ""){
+	alert("Please provide your mobile number!");
+	document.f_signup.mobono.focus();
+	return false;
+	}
+
+	if(document.f_signup.password.value == ""){
+	alert("Please set a password!");
+	document.f_signup.password.focus();
+	return false;
+	}
+
+	return (true);
+}
